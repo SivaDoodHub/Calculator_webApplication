940f08a2d9cf554ed4c1f601a8c9b2dbd26a7ed6
SivaDoodHub <sivarohit456@gmail.com>
2025-07-05 02:48:47 +0530

    merged changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/ModernCalculatorWeb/',
+  plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
